cd5ef0b8694c3046450c3854fc9ba7b6dc9fa69e
Cicero Junior <cicerojunior.cont@gmail.com>
2025-02-25 18:37:31 +0000

    feat(errors): add custom application error handling and error enumeration

---
+++ src/application/create_customer.py
@@ -0,0 +1,32 @@
+
+from dataclasses import dataclass
+from src.application.errors.application_errors_enum import ApplicationErrors
+from src.domain.entities.customer import Customer
+from src.domain.interfaces.id_generator import IdGenerator
+from src.domain.interfaces.repositories.customer_repository import CustomerRepository
+from src.application.errors.application_error import ApplicationError
+
+@dataclass
+class CreateCustomerInput:
+    name: str
+    email: str
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(**data)
+
+class CreateCustomer:
+    def __init__(self, id_gen: IdGenerator, customer_repo: CustomerRepository):
+        self.customer_repo = customer_repo
+        self.id_gen = id_gen
+
+    def execute(self, input: CreateCustomerInput) -> str:
+        '''
+        Creates a new Customer if not exists
+        :param input: CreateCustomerInput
+        return ID: str
+        '''
+        if(self.customer_repo.email_exists(input.email)):
+            raise ApplicationError(ApplicationErrors.CONFLICT, "Customer already exists")
+        customer = Customer(id=self.id_gen.generate(), name=input.name, email=input.email)
+        ID = self.customer_repo.save(customer=customer)
+        return ID
