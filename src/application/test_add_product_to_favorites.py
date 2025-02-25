eb94eb4d31d8a2bb92b3e6776f6fc35909948bde
Cicero Junior <cicerojunior.cont@gmail.com>
2025-02-25 20:30:02 +0000

    test: enhance AddProductToFavorites unit tests with favorite product configuration

--- src/application/test_add_product_to_favorites.py
+++ src/application/test_add_product_to_favorites.py
@@ -6,6 +6,7 @@ from src.application.errors.application_error import ApplicationError
 from src.application.errors.application_errors_enum import ApplicationErrors
 from src.domain.entities.customer import Customer
 
+from src.domain.entities.favorite_product import FavoriteProduct
 from src.domain.interfaces.gateway.product_service import ProductOutput, ProductService
 from src.domain.interfaces.id_generator import IdGenerator
 from src.domain.interfaces.repositories.customer_repository import CustomerRepository
@@ -16,25 +17,36 @@ CUSTOMER_ID='eefc9802-1d7d-4aec-8190-8838ed66fbba'
 PRODUCT_ID='20d4e197-a909-4c37-a92b-2bb0ddcd814f'
 FAVORITE_PRODUCT_ID = '246a7903-056a-4a40-b3d1-ad406a891b83'
 
-def __config_default_mocks(mock_customer_repository, mock_favorite_product_repository, mock_id_generator, mock_product_service):
-    # Mocking services
-    mock_customer_repository.find_by_id = AsyncMock()
-    mock_customer_repository.find_by_id.return_value = Customer.from_dict({
+customer = {
         'id': CUSTOMER_ID,
         'name': 'customer_name',
         'email': 'customer_email@mail.com'
-    })
+    }
+product = {
+        'id': PRODUCT_ID,
+        'title': 'PRODUCT_1',
+        'image': 'https://luizalabs.com/api/images/1',
+        'price': 100.0
+    }
+favorite_product = {
+        'id': FAVORITE_PRODUCT_ID,
+        'customer_id': CUSTOMER_ID,
+        'product_id': product["id"],
+        'title': product["title"],
+        'image': product["image"],
+        'price': product["price"]
+    }
+
+def __config_default_mocks(mock_customer_repository, mock_favorite_product_repository, mock_id_generator, mock_product_service):
+    # Mocking services
+    mock_customer_repository.find_by_id = AsyncMock()
+    mock_customer_repository.find_by_id.return_value = Customer.from_dict(customer)
     mock_favorite_product_repository.exists_product_in_customer_favorites = AsyncMock()
     mock_favorite_product_repository.exists_product_in_customer_favorites.return_value = False
     mock_id_generator.generate.return_value = FAVORITE_PRODUCT_ID
     mock_favorite_product_repository.add.return_value = FAVORITE_PRODUCT_ID
     mock_product_service.get = AsyncMock()
-    mock_product_service.get.return_value = ProductOutput.from_dict({
-        'id': PRODUCT_ID,
-        'title': 'PRODUCT_1',
-        'image': 'https://luizalabs.com/api/images/1',
-        'price': 100.0
-    })
+    mock_product_service.get.return_value = ProductOutput.from_dict(product)
 
 @patch('src.domain.interfaces.repositories.customer_repository.CustomerRepository')
 @patch('src.domain.interfaces.repositories.favorite_product_repository.FavoriteProductRepository')
@@ -55,9 +67,12 @@ async def test_should_add_product_to_favorites(mock_customer_repository: Custome
         }))
 
     mock_customer_repository.find_by_id.assert_called_once()
+    mock_customer_repository.find_by_id.assert_called_with(CUSTOMER_ID)
     mock_favorite_product_repository.exists_product_in_customer_favorites.assert_called_once()
+    mock_favorite_product_repository.exists_product_in_customer_favorites.assert_called_with(CUSTOMER_ID, PRODUCT_ID)
     mock_id_generator.generate.assert_called_once()
     mock_favorite_product_repository.add.assert_called_once()
+    mock_favorite_product_repository.add.assert_called_with(FavoriteProduct.from_dict(favorite_product))
     mock_product_service.get.assert_called_once()
     assert favorite_product_id == FAVORITE_PRODUCT_ID
 
